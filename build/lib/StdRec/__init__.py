c6bc0529b83dab7019e1eece2ec41bd2aaf812b7
Zangshihui <zangsh20@gmail.com>
2022-10-30 23:46:21 -0400

    remove some rubbish

--- build/lib/StdRec/__init__.py
+++
@@ -1,6 +0,0 @@
-from StdRec.Reconstruct import Reconstruct
-from StdRec.Fast import Fast
-from StdRec.Base import Base
-from StdRec import readgadget
-from StdRec import readfof
-from StdRec import readsnap
